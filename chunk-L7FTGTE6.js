fc549e8e3abdb9e080b65760521943bdd0da2aca
Ricardo Fernandez Vilca <ricardofervil@gmail.com>
2025-06-16 09:01:59 +0200

    Auto-generated commit

---
+++ chunk-L7FTGTE6.js
@@ -0,0 +1,1 @@
+import"./chunk-ACKELEN3.js";var o=[{path:"sign-in",loadComponent:()=>import("./chunk-64O2NKRW.js")},{path:"sign-up",loadComponent:()=>import("./chunk-3WMY5NGB.js")}];export{o as default};
